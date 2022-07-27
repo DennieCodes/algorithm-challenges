53da078380c9aaeb4a7af004bd9fa7e83d729378
Oculareo <oculareo@gmail.com>
2022-07-27 08:56:02 -0400

    Removed LinkedIn challenge course files

--- LinkedIn/Javascript Async/Ex_Files_JavaScript_Async_2021/Exercise Files/Ch01/01_03/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Hi!");
-
-setTimeout(function() {
-    console.log("Asynchronous result");
-}, 5000);
-
-console.log("Synchronous result.");
--- LinkedIn/Javascript Code Challenges/Coffee Calculations/begin.js
+++
@@ -1,9 +0,0 @@
-const coffeeTally = (total) => {
-	const tally = total.reduce((prev, cur) => {
-		return prev + cur * 1.25;
-	}, 0);
-
-	return `The total bill is, $${tally}`;
-};
-
-console.log(coffeeTally([1, 2, 3]));
