bb28d10b5e388d368dce82be4ea24181665d5dcc
Dennie <oculareo@gmail.com>
2023-01-08 07:38:25 -0500

    Added Codewars Survive the Attack

--- Codewars/surviveTheAttack.js
+++ Codewars/surviveTheAttack.js
@@ -61,22 +61,13 @@ const surviveTheAttack = (att, def) => {
 	}
 };
 
-// let result = surviveTheAttack([1, 3, 5, 7], [2, 4, 6, 8]);
-// console.log(result);
-
 // attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 6, 8 ]
 // //0 survivors                4 survivors
 // //return true
 
-// const result = surviveTheAttack([1, 3, 5, 7], [2, 4]);
-// console.log(result);
-
 // attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4 ]
 // //2 survivors  (16 damage)   2 survivors (6 damage)
 // //return false
-
-const result = surviveTheAttack([1, 3, 5, 7], [2, 4, 0, 8]);
-console.log(result);
 
 // attackers=[ 1, 3, 5, 7 ]   defenders=[ 2, 4, 0, 8 ]
 // //1 survivors                3 survivors
